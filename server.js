93f20d36d3093798d4a7e05bfcadb02d470d9f59
Neskwik93 <eric.bauman@laposte.net>
2019-07-15 15:02:44 +0200

    .

--- server.js
+++ server.js
@@ -10,16 +10,40 @@ var server = app.listen(3000, () => {
 });
 var io = socketio(server);
 
-
+var ballSize = 15;
 var boardWidth = 1100;
 var boardHeight = 600;
-var ballPosition = { 'x': (boardWidth / 2)+7.5, 'y': (boardHeight / 2)-7.5 };
+var ballPosition = { x: (boardWidth / 2) - (ballSize / 2), y: (boardHeight / 2) - (ballSize / 2) };
+var vitesse = { x: 0, y: 0 };
 
 io.on('connection', client => {
-    client.emit('ballPosition', ballPosition);
+    init();
+    let interval = setInterval(() => {
+        ballPosition.x += vitesse.x; //direction aleatoire plus tard
+        ballPosition.y += vitesse.y;
+
+        if (ballPosition.y > 0) {
+            client.emit('ballPosition', ballPosition);
+        } else {
+            collision();
+        }
+        if (ballPosition.y > 600) {
+            client.emit('ballPosition', ballPosition);
+        } else {
+            collision();
+        }
+    }, 20)
 
 
 
 
 
 });
+
+function collision() {
+ //changer la vitesse pour redefinir la direction de la ball
+}
+
+function init() {
+    vitesse = { x: -1, y: 0 };
+}
