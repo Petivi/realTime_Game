3c2e66770ff3e8eef81edf9b42d5dfc532db0467
Petivi <33059675+Petivi@users.noreply.github.com>
2019-07-15 14:25:31 +0200

    Revert "."
    This reverts commit b83936e4c54f95c51993ddfe036ba3066c4b3287.

--- server.js
+++ server.js
@@ -9,17 +9,3 @@ var server = app.listen(3000, () => {
     console.log('serveur ecoutant sur le port 3000...')
 });
 var io = socketio(server);
-
-
-var boardWidth = 1100;
-var boardHeight = 600;
-var ballPosition = {'x': boardWidth/2, 'y': boardHeight/2};
-
-io.on('connection', client => {
-    client.emit('ballPosition', ballPosition);
-
-
-
-
-
-});
